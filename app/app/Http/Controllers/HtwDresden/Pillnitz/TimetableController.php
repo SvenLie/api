b08b87a539ba99e9362ebe7563a6ba27a38bdd02
Sven Liebert <mail@sven-liebert.de>
2022-01-31 19:10:52 +0100

    API - HTW DD Pillnitz: Fix problems

--- app/app/Http/Controllers/HtwDresden/Pillnitz/TimetableController.php
+++ app/app/Http/Controllers/HtwDresden/Pillnitz/TimetableController.php
@@ -71,55 +71,171 @@ class TimetableController extends Controller
             for ($j = 1; $j < $tableRow->childNodes->count(); $j++) {
                 $cell = $tableRow->childNodes->item($j);
 
-                if ($cell->childNodes->count() != 6 && $cell->childNodes->count() != 7) {
+                if ($cell->childNodes->count() != 6 && $cell->childNodes->count() != 7 && $cell->childNodes->count() != 14 && $cell->childNodes->count() != 13) {
                     continue;
                 }
 
-                $lecture = new Lecture();
-                if($cell->childNodes->count() == 7) {
-                    $lecture->setModule($cell->childNodes->item(1)->childNodes->item(0)->nodeValue . ' ' . $cell->childNodes->item(1)->childNodes->item(2)->nodeValue);
-                    $lecture->setModuleNumber(substr($cell->childNodes->item(3)->nodeValue, 0, 4));
+                // zwei lectures gleichzeitig
+                if ($cell->childNodes->count() == 14 || $cell->childNodes->count() == 13) {
+                    $lectureOne = new Lecture();
+                    $lectureTwo = new Lecture();
+
+                    $lectureOne->setModule($cell->childNodes->item(1)->childNodes->item(0)->nodeValue);
+
+                    if($cell->childNodes->count() != 13) {
+                        if(!empty($cell->childNodes->item(8)->childNodes->item(0)->nodeValue)) {
+                            $lectureTwo->setModule($cell->childNodes->item(8)->childNodes->item(0)->nodeValue);
+                            $lectureTwo->setModuleNumber(substr($cell->childNodes->item(8)->childNodes->item(2)->nodeValue, 0, 4));
+                            $lectureTwo->setType($cell->childNodes->item(10)->nodeValue);
+                            if ($cell->childNodes->item(8)->childNodes->count() > 3 && $cell->childNodes->item(8)->childNodes->item(3)->hasAttributes()) {
+                                $lectureTwo->setLink($cell->childNodes->item(8)->childNodes->item(3)->attributes->item(0)->nodeValue);
+                            } else {
+                                $lectureTwo->setLink("");
+                            }
+                        } else {
+                            $lectureTwo->setModule($cell->childNodes->item(9)->childNodes->item(0)->nodeValue);
+                            $lectureTwo->setModuleNumber(substr($cell->childNodes->item(9)->childNodes->item(2)->nodeValue, 0, 4));
+                            $lectureTwo->setType($cell->childNodes->item(11)->nodeValue);
+                            if ($cell->childNodes->item(9)->childNodes->count() > 3 && $cell->childNodes->item(9)->childNodes->item(3)->hasAttributes()) {
+                                $lectureTwo->setLink($cell->childNodes->item(9)->childNodes->item(3)->attributes->item(0)->nodeValue);
+                            } else {
+                                $lectureTwo->setLink("");
+                            }
+                        }
+
+                    } else {
+                        if(!empty($cell->childNodes->item(9)->childNodes->item(0)->nodeValue)) {
+                            $lectureTwo->setModule($cell->childNodes->item(9)->childNodes->item(0)->nodeValue);
+                            $lectureTwo->setModuleNumber(substr($cell->childNodes->item(9)->childNodes->item(2)->nodeValue, 0, 4));
+                            $lectureTwo->setType($cell->childNodes->item(11)->nodeValue);
+                            if ($cell->childNodes->item(9)->childNodes->count() > 3 && $cell->childNodes->item(9)->childNodes->item(3)->hasAttributes()) {
+                                $lectureTwo->setLink($cell->childNodes->item(9)->childNodes->item(3)->attributes->item(0)->nodeValue);
+                            } else {
+                                $lectureTwo->setLink("");
+                            }
+                        } else {
+                            $lectureTwo->setModule($cell->childNodes->item(8)->childNodes->item(0)->nodeValue);
+                            $lectureTwo->setModuleNumber(substr($cell->childNodes->item(8)->childNodes->item(2)->nodeValue, 0, 4));
+                            $lectureTwo->setType($cell->childNodes->item(10)->nodeValue);
+                            if ($cell->childNodes->item(8)->childNodes->count() > 3 && $cell->childNodes->item(8)->childNodes->item(3)->hasAttributes()) {
+                                $lectureTwo->setLink($cell->childNodes->item(8)->childNodes->item(3)->attributes->item(0)->nodeValue);
+                            } else {
+                                $lectureTwo->setLink("");
+                            }
+                        }
+
+                    }
+
+                    $lectureOne->setModuleNumber(substr($cell->childNodes->item(1)->childNodes->item(2)->nodeValue, 0, 4));
+                    $lectureOne->setType($cell->childNodes->item(3)->nodeValue);
+
+                    if ($cell->childNodes->item(1)->childNodes->count() > 3 && $cell->childNodes->item(1)->childNodes->item(3)->hasAttributes()) {
+                        $lectureOne->setLink($cell->childNodes->item(1)->childNodes->item(3)->attributes->item(0)->nodeValue);
+                    } else {
+                        $lectureOne->setLink("");
+                    }
+
+                    $lectureOne->setStartingTimestamp(strtotime($year . "W" . $week . " " . $this->times[$i]["start"] . "+" . ($j - 1) . " day"));
+                    $lectureTwo->setStartingTimestamp(strtotime($year . "W" . $week . " " . $this->times[$i]["start"] . "+" . ($j - 1) . " day"));
+                    $lectureOne->setEndingTimestamp(strtotime($year . "W" . $week . " " . $this->times[$i]["end"] . "+" . ($j - 1) . " day"));
+                    $lectureTwo->setEndingTimestamp(strtotime($year . "W" . $week . " " . $this->times[$i]["end"] . "+" . ($j - 1) . " day"));
+                    if ($lectureOne->getStartingTimestamp() == "" || $lectureOne->getEndingTimestamp() == "" || $lectureTwo->getStartingTimestamp() == "" || $lectureTwo->getEndingTimestamp() == "") {
+                        return response()->json(['error' => 'Please check your week and year combination'], 500);
+                    }
+
+                    // ignore this stuff for now
+                    $lectureOne->setPlace("");
+                    $lectureTwo->setPlace("");
+                    $lectureOne->setLecturer("");
+                    $lectureTwo->setLecturer("");
+
+                    $lectures[] = [
+                        "module" => mb_detect_encoding($lectureOne->getModule()) == "UTF-8" ? utf8_decode($lectureOne->getModule()) : $lectureOne->getModule(),
+                        "moduleNumber" => mb_detect_encoding($lectureOne->getModuleNumber()) == "UTF-8" ? utf8_decode($lectureOne->getModuleNumber()) : $lectureOne->getModuleNumber(),
+                        "link" => $lectureOne->getLink(),
+                        "type" => mb_detect_encoding($lectureOne->getType()) == "UTF-8" ? utf8_decode($lectureOne->getType()) : $lectureOne->getType(),
+                        "place" => mb_detect_encoding($lectureOne->getPlace()) == "UTF-8" ? utf8_decode($lectureOne->getPlace()) : $lectureOne->getPlace(),
+                        "lecturer" => mb_detect_encoding($lectureOne->getLecturer()) == "UTF-8" ? utf8_decode($lectureOne->getLecturer()) : $lectureOne->getLecturer(),
+                        "startingTimestamp" => $lectureOne->getStartingTimestamp(),
+                        "endingTimestamp" => $lectureOne->getEndingTimestamp(),
+                    ];
+
+                    $lectures[] = [
+                        "module" => mb_detect_encoding($lectureTwo->getModule()) == "UTF-8" ? utf8_decode($lectureTwo->getModule()) : $lectureTwo->getModule(),
+                        "moduleNumber" => mb_detect_encoding($lectureTwo->getModuleNumber()) == "UTF-8" ? utf8_decode($lectureTwo->getModuleNumber()) : $lectureTwo->getModuleNumber(),
+                        "link" => $lectureTwo->getLink(),
+                        "type" => mb_detect_encoding($lectureTwo->getType()) == "UTF-8" ? utf8_decode($lectureTwo->getType()) : $lectureTwo->getType(),
+                        "place" => mb_detect_encoding($lectureTwo->getPlace()) == "UTF-8" ? utf8_decode($lectureTwo->getPlace()) : $lectureTwo->getPlace(),
+                        "lecturer" => mb_detect_encoding($lectureTwo->getLecturer()) == "UTF-8" ? utf8_decode($lectureTwo->getLecturer()) : $lectureTwo->getLecturer(),
+                        "startingTimestamp" => $lectureTwo->getStartingTimestamp(),
+                        "endingTimestamp" => $lectureTwo->getEndingTimestamp(),
+                    ];
 
                 } else {
-                    $lecture->setModule($cell->childNodes->item(1)->childNodes->item(0)->nodeValue);
-                    $lecture->setModuleNumber(substr($cell->childNodes->item(1)->childNodes->item(2)->nodeValue, 0, 4));
+                    $lecture = new Lecture();
+                    if($cell->childNodes->count() == 7) {
+
+                        if (preg_match("/[A-Z]{1}[0-9]{3}/",substr($cell->childNodes->item(3)->nodeValue, 0, 4))) {
+                            $lecture->setModule($cell->childNodes->item(1)->childNodes->item(0)->nodeValue . ' ' . $cell->childNodes->item(1)->childNodes->item(2)->nodeValue);
+                            $lecture->setModuleNumber(substr($cell->childNodes->item(3)->nodeValue, 0, 4));
+                            $lecture->setType(substr($cell->childNodes->item(3)->nodeValue, strpos($cell->childNodes->item(3)->nodeValue," -") + 3, strrpos($cell->childNodes->item(3)->nodeValue,"-")));
+                        } else {
+                            $lecture->setType($cell->childNodes->item(3)->nodeValue);
+                            $lecture->setModule($cell->childNodes->item(1)->childNodes->item(0)->nodeValue);
+                            if (preg_match("/[A-Z]{1}[0-9]{3}/",substr($cell->childNodes->item(1)->childNodes->item(2)->nodeValue, 0, 4))) {
+                                $lecture->setModuleNumber(substr($cell->childNodes->item(1)->childNodes->item(2)->nodeValue, 0, 4));
+                            } else {
+                                $lecture->setModule($cell->childNodes->item(1)->childNodes->item(0)->nodeValue . " " . substr($cell->childNodes->item(1)->childNodes->item(2)->nodeValue, 0,strlen($cell->childNodes->item(1)->childNodes->item(2)->nodeValue)-7));
+                                $lecture->setModuleNumber(substr($cell->childNodes->item(1)->childNodes->item(2)->nodeValue, -4));
+                            }
+
+                        }
+                    } else {
+                        $lecture->setType($cell->childNodes->item(3)->nodeValue);
+                        if (preg_match("/[A-Z]{1}[0-9]{3}/",substr($cell->childNodes->item(1)->childNodes->item(2)->nodeValue, 0, 4))) {
+                            $lecture->setModuleNumber(substr($cell->childNodes->item(1)->childNodes->item(2)->nodeValue, 0, 4));
+                            $lecture->setModule($cell->childNodes->item(1)->childNodes->item(0)->nodeValue);
+                        } else {
+                            $lecture->setModuleNumber(substr($cell->childNodes->item(1)->childNodes->item(2)->nodeValue,strrpos($cell->childNodes->item(1)->childNodes->item(2)->nodeValue,"-") + 2, 4));
+                            $lecture->setModule($cell->childNodes->item(1)->childNodes->item(0)->nodeValue . " " . substr($cell->childNodes->item(1)->childNodes->item(2)->nodeValue,0,strrpos($cell->childNodes->item(1)->childNodes->item(2)->nodeValue," -")));
+                        }
+
+                    }
+
+
+                    if ($cell->childNodes->item(1)->childNodes->count() > 3 && $cell->childNodes->item(1)->childNodes->item(3)->hasAttributes()) {
+                        $lecture->setLink($cell->childNodes->item(1)->childNodes->item(3)->attributes->item(0)->nodeValue);
+                    } else {
+                        $lecture->setLink("");
+                    }
+                    $lecture->setStartingTimestamp(strtotime($year . "W" . $week . " " . $this->times[$i]["start"] . "+" . ($j - 1) . " day"));
+                    $lecture->setEndingTimestamp(strtotime($year . "W" . $week . " " . $this->times[$i]["end"] . "+" . ($j - 1) . " day"));
+
+                    if ($lecture->getStartingTimestamp() == "" || $lecture->getEndingTimestamp() == "") {
+                        return response()->json(['error' => 'Please check your week and year combination'], 500);
+                    }
+
+                    if($cell->childNodes->count() == 7) {
+                        $place = $cell->childNodes->item(5)->nodeValue . substr($cell->childNodes->item(6)->nodeValue, 0, strpos($cell->childNodes->item(6)->nodeValue, " -"));
+                        $lecturer = substr($cell->childNodes->item(6)->nodeValue, strrpos($cell->childNodes->item(6)->nodeValue, "- ") + 2);
+                    } else {
+                        $place = substr($cell->childNodes->item(5)->nodeValue, 0, strpos($cell->childNodes->item(5)->nodeValue, " -"));
+                        $lecturer = substr($cell->childNodes->item(5)->nodeValue, strrpos($cell->childNodes->item(5)->nodeValue, "- ") + 2);
+                    }
+                    $lecture->setPlace($place);
+                    $lecture->setLecturer($lecturer);
+
+                    $lectures[] = [
+                        "module" => mb_detect_encoding($lecture->getModule()) == "UTF-8" ? utf8_decode($lecture->getModule()) : $lecture->getModule(),
+                        "moduleNumber" => mb_detect_encoding($lecture->getModuleNumber()) == "UTF-8" ? utf8_decode($lecture->getModuleNumber()) : $lecture->getModuleNumber(),
+                        "link" => $lecture->getLink(),
+                        "type" => mb_detect_encoding($lecture->getType()) == "UTF-8" ? utf8_decode($lecture->getType()) : $lecture->getType(),
+                        "place" => mb_detect_encoding($lecture->getPlace()) == "UTF-8" ? utf8_decode($lecture->getPlace()) : $lecture->getPlace(),
+                        "lecturer" => mb_detect_encoding($lecture->getLecturer()) == "UTF-8" ? utf8_decode($lecture->getLecturer()) : $lecture->getLecturer(),
+                        "startingTimestamp" => $lecture->getStartingTimestamp(),
+                        "endingTimestamp" => $lecture->getEndingTimestamp(),
+                    ];
                 }
-
-
-                if ($cell->childNodes->item(1)->childNodes->count() > 3 && $cell->childNodes->item(1)->childNodes->item(3)->hasAttributes()) {
-                    $lecture->setLink($cell->childNodes->item(1)->childNodes->item(3)->attributes->item(0)->nodeValue);
-                } else {
-                    $lecture->setLink("");
-                }
-                $lecture->setType($cell->childNodes->item(3)->nodeValue);
-                $lecture->setStartingTimestamp(strtotime($year . "W" . $week . " " . $this->times[$i]["start"] . "+" . ($j - 1) . " day"));
-                $lecture->setEndingTimestamp(strtotime($year . "W" . $week . " " . $this->times[$i]["end"] . "+" . ($j - 1) . " day"));
-
-                if ($lecture->getStartingTimestamp() == "" || $lecture->getEndingTimestamp() == "") {
-                    return response()->json(['error' => 'Please check your week and year combination'], 500);
-                }
-
-                if($cell->childNodes->count() == 7) {
-                    $place = $cell->childNodes->item(5)->nodeValue . substr($cell->childNodes->item(6)->nodeValue, 0, strpos($cell->childNodes->item(6)->nodeValue, " -"));
-                    $lecturer = substr($cell->childNodes->item(6)->nodeValue, strrpos($cell->childNodes->item(6)->nodeValue, "- ") + 2);
-                } else {
-                    $place = substr($cell->childNodes->item(5)->nodeValue, 0, strpos($cell->childNodes->item(5)->nodeValue, " -"));
-                    $lecturer = substr($cell->childNodes->item(5)->nodeValue, strrpos($cell->childNodes->item(5)->nodeValue, "- ") + 2);
-                }
-                $lecture->setPlace($place);
-                $lecture->setLecturer($lecturer);
-
-                $lectures[] = [
-                    "module" => mb_detect_encoding($lecture->getModule()) == "UTF-8" ? utf8_decode($lecture->getModule()) : $lecture->getModule(),
-                    "moduleNumber" => mb_detect_encoding($lecture->getModuleNumber()) == "UTF-8" ? utf8_decode($lecture->getModuleNumber()) : $lecture->getModuleNumber(),
-                    "link" => $lecture->getLink(),
-                    "type" => mb_detect_encoding($lecture->getType()) == "UTF-8" ? utf8_decode($lecture->getType()) : $lecture->getType(),
-                    "place" => mb_detect_encoding($lecture->getPlace()) == "UTF-8" ? utf8_decode($lecture->getPlace()) : $lecture->getPlace(),
-                    "lecturer" => mb_detect_encoding($lecture->getLecturer()) == "UTF-8" ? utf8_decode($lecture->getLecturer()) : $lecture->getLecturer(),
-                    "startingTimestamp" => $lecture->getStartingTimestamp(),
-                    "endingTimestamp" => $lecture->getEndingTimestamp(),
-                ];
-
             }
         }
         return response()->json($lectures);
