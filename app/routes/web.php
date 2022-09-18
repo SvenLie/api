93198fd1e64f3332634726a8a6a27de920172525
Sven Liebert <mail@sven-liebert.de>
2022-09-18 15:43:55 +0200

    API - Pick and ban: Initial commit

--- app/routes/web.php
+++ app/routes/web.php
@@ -19,8 +19,14 @@ $router->get('/', function () use ($router) {
         if ($value['uri'] == '/') {
             continue;
         }
-        echo $value['uri'] . " (". $value['method'] . ")<br />";
+        echo $value['uri'] . " (" . $value['method'] . ")<br />";
     }
+});
+
+$router->group(['prefix' => 'auth'], function () use ($router) {
+    $router->post('login', 'AuthController@login');
+    $router->post('logout', 'AuthController@logout');
+    $router->post('register', 'AuthController@register');
 });
 
 $router->group(['prefix' => 'htw-dresden'], function () use ($router) {
@@ -45,13 +51,13 @@ $router->group(['prefix' => 'htw-dresden'], function () use ($router) {
     });
 });
 
-$router->group(['prefix' => 'date-time'], function() use ($router) {
+$router->group(['prefix' => 'date-time'], function () use ($router) {
     $router->get('/current-time', [
         'as' => 'current-time', 'uses' => 'DateTime\TimeController@getCurrentTimeInGermany'
     ]);
 });
 
-$router->group(['prefix' => 'automation'], function() use ($router) {
+$router->group(['prefix' => 'automation'], function () use ($router) {
     $router->get('/presence', [
         'as' => 'presence', 'uses' => 'Automation\PresenceController@getCurrentPresenceStatus'
     ]);
@@ -59,4 +65,41 @@ $router->group(['prefix' => 'automation'], function() use ($router) {
     $router->post('/presence', [
         'as' => 'presence', 'uses' => 'Automation\PresenceController@setCurrentPresenceStatus'
     ]);
+});
+
+
+$router->group(['prefix' => 'pick-and-ban'], function () use ($router) {
+    $router->group(['middleware' => ['auth', 'hasRolePickAndBan']], function () use ($router) {
+        # GET ROUTES
+        $router->get('/games', [
+            'as' => 'games', 'uses' => 'PickAndBan\GameController@getOwnGames'
+        ]);
+        $router->get('/game-sets', [
+            'as' => 'game-sets', 'uses' => 'PickAndBan\GameSetController@getOwnGameSets'
+        ]);
+        $router->get('/game-set-items', [
+            'as' => 'game-set-items', 'uses' => 'PickAndBan\GameSetItemController@getAllGameSetItemsByGame'
+        ]);
+        $router->get('/rule-sets', [
+            'as' => 'rule-sets', 'uses' => 'PickAndBan\RuleSetController@getOwnRuleSets'
+        ]);
+
+        # CREATE ROUTES
+        $router->put('/game', 'PickAndBan\GameController@createGame');
+        $router->put('/game-set', 'PickAndBan\GameSetController@createGameSet');
+        $router->put('/game-set-item', 'PickAndBan\GameSetItemController@createGameSetItem');
+        $router->put('/rule-set', 'PickAndBan\RuleSetController@createRuleSet');
+
+        # DELETE ROUTES
+        $router->delete('/game', 'PickAndBan\GameController@deleteGame');
+        $router->delete('/game-set', 'PickAndBan\GameSetController@deleteGameSet');
+        $router->delete('/game-set-item', 'PickAndBan\GameSetItemController@deleteGameSetItem');
+        $router->delete('/rule-set', 'PickAndBan\RuleSetController@deleteRuleSet');
+
+        # EDIT ROUTES
+        $router->post('/game', 'PickAndBan\GameController@editGame');
+        $router->post('/game-set', 'PickAndBan\GameSetController@editGameSet');
+        $router->post('/game-set-item', 'PickAndBan\GameSetItemController@editGameSetItem');
+        $router->post('/rule-set', 'PickAndBan\RuleSetController@editRuleSet');
+    });
 });
